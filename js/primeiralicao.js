d22ecc535544c0d8d29634bc4e10ae8194a02164
Castor Troy <castor@troynote>
2015-03-25 22:27:52 +1300

    Criando meu CV

---
+++ js/primeiralicao.js
@@ -0,0 +1,88 @@
+/*
+Instructor Notes
+
+work contains an array of jobs. Each job object in jobs should contain an employer, title, location, dates worked and description.
+
+projects contains an array of projects. Each project object in projects should contain a title, dates worked, description, and an images array with URL strings for project images.
+
+bio contains a name, role, welcomeMessage, contacts object and skills array. The contacts object should contain (but doesn't have to) a mobile number, email address, github username, twitter handle and location.
+
+education contains an array of schools. Each school object in schools contains a name, location, degree, majors array, dates attended and a url for the school's website. education also contains an onlineCourses array. Each onlineCourse object in onlineCourses should contain a title, school, dates attended and a url for the course.
+*/
+/*aprendendo*/
+/*$("#main").append("Joao Lucio")
+var awesomeThoughts="I am Lucio and I am AWESOME";
+console.log(awesomeThoughts);
+var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+$("#main").append(funThoughts);
+console.log(funThoughts);*/
+
+/*aprendendo*/
+var name = "Joao Lucio";
+var role = "Software Developer"
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+//var formattedRole="Software Developer";
+var skill = ["Java","Linux"];
+var testeBio = {
+  "name" : "Lucio",
+  "age" : 39,
+  "role" : "Software Developer",
+  "contact" : {
+    "email" : "luciomoraes@gmail.com",
+    "mobile" : "02108215335",
+    "github" : "C4storTroy",
+    "location" : "Wellington, New Zealand"
+  },
+  "welMessage" : "Welcome",
+  "Skills" : skill,
+  "pic" : "./images/lucio.png"
+};
+
+$("#main").append(testeBio.name);
+$("#main").append(testeBio.age);
+//$("#main").append(testeBio.role);
+$("#main").append(testeBio.pic);
+
+var workObj = {
+  "position" : "Software Developer",
+  "company" : "Catalyst IT ltd",
+  "yearsWorked" : 1,
+};
+workObj.city = "Wellington";
+
+var eduObj = {
+  "yearsSchool" : 7,
+};
+eduObj["name"] = "UEA";
+
+$("#main").append(workObj["position"]);
+$("#main").append(eduObj.name);
+
+var education = {
+  "schools":[
+    {
+      "name" : "UEA",
+      "city" : "Manaus",
+      "degree" : "BA",
+      "major" : "Computer Engineering"
+    },
+    {
+      "name" : "Fucapi",
+      "city" : "Manaus",
+      "degree" : "Specialist",
+      "major" : ["Software", "Engineering"]
+    }
+  ],
+ "onlineCourses" : [
+   {
+     "title" : "Front Web Developer",
+     "school" : "Udacity",
+     "date" : 2014,
+     "url" : "www.udacity.com"
+   }
+ ]
+}
---
+++ js/resumeBuilder.js
@@ -0,0 +1,63 @@
+var bio = {
+  "name" : "Lucio",
+  "age" : 39,
+  "role" : "Software Developer",
+  "contact" : {
+    "email" : "luciomoraes@gmail.com",
+    "mobile" : "02108215335",
+    "github" : "C4storTroy",
+    "location" : "Wellington, New Zealand"
+  },
+  "welMessage" : "Welcome",
+  "Skills" : skill,
+  "pic" : "./images/lucio.png"
+};
+var work = {};
+  "position" : "Software Developer",
+  "company" : "Catalyst IT ltd",
+  "yearsWorked" : 1,
+  "city" : "Wellington"
+};
+
+var education = {
+  "schools":[
+    {
+      "name" : "UEA",
+      "city" : "Manaus",
+      "degree" : "BA",
+      "major" : "Computer Engineering"
+    },
+    {
+      "name" : "Fucapi",
+      "city" : "Manaus",
+      "degree" : "Specialist",
+      "major" : ["Software", "Engineering"]
+    }
+  ],
+ "onlineCourses" : [
+   {
+     "title" : "Front Web Developer",
+     "school" : "Udacity",
+     "date" : 2014,
+     "url" : "www.udacity.com"
+   }
+ ]
+};
+
+var project = {
+  [
+    {
+      "title" : "Educational Game",
+      "company" : "ICA",
+      "date" : 2007,
+      "url" : "www.certiamazonia.org.br"
+    },
+    {
+      "title" : "Software Test",
+      "school" : "Fucapi",
+      "date" : 2010,
+      "url" : "www.fucapi.com"
+    }
+  ]
+
+};
